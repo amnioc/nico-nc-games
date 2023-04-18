081673c7ee9b4939e381589ef62d32cc7fadb145
amnioc <nicdhillon19@gmail.com>
2023-04-18 10:15:39 +0100

    moved useEffect lower to AllReviews component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Home from "./views/Home.jsx";
-import { useState, useEffect } from "react";
-import { getAllReviews } from "./api";
+import { useState } from "react";
 
 function App() {
   const [reviews, setReviews] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getAllReviews().then((response) => {
-      setIsLoading(true);
-      setReviews(response.data.reviews);
-      setIsLoading(false);
-    });
-  }, []);
-
-  if (isLoading) {
-    return <h4 id="loadingMessage">Reviews Loading...</h4>;
-  }
 
   return (
     <div className="App">
--- src/components/AllReviews.jsx
+++ src/components/AllReviews.jsx
@@ -1,6 +1,22 @@
 import ReviewCards from "./ReviewCards";
+import { getAllReviews } from "../api";
+import { useEffect, useState } from "react";
 
 const AllReviews = ({ reviews, setReviews }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getAllReviews().then((response) => {
+      setIsLoading(true);
+      setReviews(response.data.reviews);
+      setIsLoading(false);
+    });
+  });
+
+  if (isLoading) {
+    return <h4 id="loadingMessage">Reviews Loading...</h4>;
+  }
+
   return (
     <ul id="reviewsList">
       {reviews.map((review) => {
